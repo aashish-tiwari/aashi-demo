f5930e00594ea23e6980ac3264ed34c0a419ef07
Aashish Tiwari <Aashish.Tiwari@bestbuy.com>
2021-05-18 21:58:43 -0500

    first commit

---
+++ aashi-demo/src/main/java/com/aashi/aashidemo/AashiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aashi.aashidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AashiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AashiDemoApplication.class, args);
+    }
+
+}
---
+++ aashi-demo/src/test/java/com/aashi/aashidemo/AashiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aashi.aashidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AashiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
